6bd7320c9609c38e87a21028c1b114ab0bed0eec
johnpope3560 <omondijpaul@gmail.com>
2025-08-16 16:51:03 +0300

    Add files via upload

---
+++ py lists.py
@@ -0,0 +1,12 @@
+my_list = [10, 20, 30, 40, 50]
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+my_list.insert(1, 15)
+my_list.extend([50,60,70])
+my_list.pop(10)
+my_list.sort(10,20,30,40,50,60,70)
+index = my_list.index(30)
+print(index)
+print(my_list)
